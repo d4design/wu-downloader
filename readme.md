2f0765096be22b91c40f75a07f72d069723753b5
d4design <32513583+d4design@users.noreply.github.com>
2020-01-21 09:39:09 +1100

    WU Downloader v1.0.0

---
+++ readme.md
@@ -0,0 +1,33 @@
+# Noise Logger
+
+## Introduction
+
+This project allows a user to download weather data from a specified Weather Underground station to a CSV file in 5 minute intervals for a specified time period.
+
+## Setup
+
+Install nodejs from nodejs.org
+
+Go to this directory and run the command
+
+```sh
+npm install
+```
+
+## Development
+All functions are contained within wu-download.js.  The required information (Station ID, Start Date, Number of days required) is sent to the program as command line arguments in the format "stationID YYYY MM D N" where YYYY MM D is the start date and N is the number of days of data required. For example:
+
+```sh
+node wu-download IHUONVIL2 2019 12 1 5
+```
+
+This will give you 5 days of data from 1st December 2019 from the station IHUONVIL2.
+
+The WeatherUnderground API also accepts dates such as 2019-12-40 as equal to 2020-01-09, therefore allowing the program to us 'number of days required' rather than a start and end date.  For example:
+
+```sh
+node wu-download IHUONVIL2 2019 12 1 40
+```
+This will give you data from 1st December 2019 to 9th January 2020 for the station IHUONVIL2.
+
+Find your desired Weather Station ID at https://www.wunderground.com/wundermap
---
+++ wu-download.js
@@ -0,0 +1,49 @@
+const rp = require('request-promise');
+const fs = require("fs");
+const async = require('async');
+var striptags = require('striptags');
+const removeEmptyLines = require("remove-blank-lines");
+
+// CMD format "node wu-download stationID YYYY MM D N" where YYY MM D is the start date of data you want, and N is the number of days you want.
+// eg. "node wu-download IDRIFF4 2019 12 17 5" gets data for 5 days from Dec 17 2019 from the station IDRIFF4.
+// The WU API accepts dates such as 2019-12-40 to be the same as 2020-01-09
+var stationID = process.argv[2];
+var year = parseInt(process.argv[3]);
+var month = parseInt(process.argv[4]);
+var dayStart = parseInt(process.argv[5]);
+var numberOfDays = parseInt(process.argv[6]);
+
+var file = "WU-Weather " + stationID + " " + year + "-" + month + "-" + dayStart + ".csv";
+
+function buildURL(day) {
+  var sourceURL = "https://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=" + stationID +"&graphspan=day&month=" + month + "&day=" + day + "&year=" + year + "&format=1"
+  return sourceURL;
+}
+
+async function saveData(url, day) {
+  options = {
+      uri: url,
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
+      }
+  }
+  await rp(options)
+      .then(function (htmlString) {
+        csvData = removeEmptyLines(striptags(htmlString));
+        fs.appendFile('results/' + file, csvData, (err) => {
+            if (err) throw err;
+            console.log('Added data for day ' + day + ' to file: ' + file);
+            return;
+        });
+      })
+      .catch(function (err) {
+          console.log('error', err);
+      });
+}
+
+(async function main(){
+  for (var i = dayStart; i < dayStart + numberOfDays; i++) {
+    sourceURL = await buildURL(i);
+    await saveData(sourceURL, i);
+  };
+})();
